056b3db506726dc9960c288b6869719cfca04f11
Clarkguinocor <152986812+Clarkguinocor@users.noreply.github.com>
2023-12-22 15:08:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def index():
 	return render_template("index.html")
 
 if __name__ == "__main__":
-	app.run(debug=True, port="8080", host="0.0.0.0")
+	app.run(debug=True, port="8080", host="127.0.0.1")
